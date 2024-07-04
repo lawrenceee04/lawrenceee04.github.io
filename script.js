9b5a60275210cdcba244e822f98f0a74a392a808
Lawrence Garcia <lawrencelg04@gmail.com>
2024-07-05 00:05:09 +0800

    moved files in their designated folders

--- script.js
+++
@@ -1,7 +0,0 @@
-function neverGonnaGiveYouUp() {
-  window.open('https://youtu.be/dQw4w9WgXcQ?si=BC00jSwc5LqieFS4', '_blank');
-}
-
-function FRS() {
-    window.open('https://www.facebook.com/profile.php?id=61551070898282', '_blank');
-}
