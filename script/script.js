89725890b3d7a72743ffe2775fd4aed4497f526a
Lawrence Garcia <lawrencelg04@gmail.com>
2024-07-05 23:24:55 +0800

    rounded corners HAHAHAHA

--- script/script.js
+++ script/script.js
@@ -2,10 +2,6 @@ function neverGonnaGiveYouUp() {
   window.open('https://youtu.be/dQw4w9WgXcQ?si=BC00jSwc5LqieFS4', '_blank');
 }
 
-function FRS() {
-    window.open('https://www.facebook.com/profile.php?id=61551070898282', '_blank');
-}
-
 function Facebook() {
   window.open('https://www.facebook.com/lawrenceee24', '_blank');
 }
