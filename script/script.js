0522a380ffab920ec58820659463e675d4651441
lawrenceee04 <lawrencelg04@gmail.com>
2024-07-25 11:59:29 +0800

    prettier format

--- script/script.js
+++ script/script.js
@@ -1,19 +1,22 @@
 function neverGonnaGiveYouUp() {
-  window.open('https://youtu.be/dQw4w9WgXcQ?si=BC00jSwc5LqieFS4', '_blank');
+  window.open("https://youtu.be/dQw4w9WgXcQ?si=BC00jSwc5LqieFS4", "_blank");
 }
 
 function Facebook() {
-  window.open('https://www.facebook.com/lawrenceee24', '_blank');
+  window.open("https://www.facebook.com/lawrenceee24", "_blank");
 }
 
 function Instagram() {
-  window.open('https://www.instagram.com/lawrencegarciaaa/', '_blank');
+  window.open("https://www.instagram.com/lawrencegarciaaa/", "_blank");
 }
 
 function LinkedIn() {
-  window.open('https://www.linkedin.com/in/lawrence-garcia-a076b1208/', '_blank');
+  window.open(
+    "https://www.linkedin.com/in/lawrence-garcia-a076b1208/",
+    "_blank"
+  );
 }
 
 function Github() {
-  window.open('https://github.com/lawrenceee04', '_blank');
+  window.open("https://github.com/lawrenceee04", "_blank");
 }
